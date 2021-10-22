bbe57d9892f101fc9e8b9d6b8445ffff552ef2d9
marcoweb <marcocarvalho.web@gmail.com>
2021-10-22 11:32:28 +0000

    Navbar

--- includes/header.php
+++ includes/header.php
@@ -6,4 +6,23 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     </head>
     <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/index.php">Biblioteca</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-expanded="false">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/index.php">Gêneros</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/list_livros.php">Livros</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <br />
         <main class="container">
